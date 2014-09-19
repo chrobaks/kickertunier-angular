b9375badaf80d47dd219fbfda730c6d2fd7c6396
Stefan Chrobak <info@netcodev.de>
2014-09-19 23:21:25 +0200

    Refactoring User

--- js/app/controller/ng-userController.js
+++ js/app/controller/ng-userController.js
@@ -2,43 +2,35 @@ if(typeof GLOBAL__VARS__APP != "undefined" && typeof GLOBAL__VARS__APP.mainApp !
     GLOBAL__VARS__APP.mainApp.controller('userController', [
         '$scope', 
         'UserFactory', 
-        'StorageFactory',
         'MessageFactory', 
-        function ($scope, UserFactory, StorageFactory, MessageFactory) {
-            
+        function ($scope, UserFactory, MessageFactory) {
+            // SCOPE VAR headertitle  show controller header titel
             $scope.headertitle = "Control User";
+            // SCOPE VAR formmsg show formular messages
             $scope.formmsg = "Neuen Mitspieler speichern";
-            
+            // SCOPE VAR userdate stores all user
             $scope.userData = [
                 {nickname: 'SChrobak', firstname: 'Stefan', secondname: 'Chrobak', id: 1},
                 {nickname: 'MChrobak', firstname: 'Max', secondname: 'Chrobak', id: 2},
                 {nickname: 'PaulM', firstname: 'Paul', secondname: 'Mustermann', id: 3},
                 {nickname: 'PaulaM', firstname: 'Paula', secondname: 'Musterfrau', id: 4}
             ];
-            
-            
+            // ADD NEW USER
             $scope.addUser = function () {
-                if(UserFactory.set_scopeAddUser($scope)){
+                if(UserFactory.set_scopeAddUser()){
                     $scope.formmsg = "Neuen Mitspieler speichern";
                 }else{
                     $scope.formmsg = MessageFactory.get_error();
                 }
             }  
-            
+            // DELETE USER
             $scope.deleteUser = function (id) {
-                if( ! StorageFactory.get_storeCheckUserIsInTeam(id)){
-                    UserFactory.set_scopeDeleteUser(id, $scope);
-                    StorageFactory.set_storeTeamUserDataScope($scope.userData);
-                }else{
-                    alert("Der Spieler ist Mitglied in einem Team und kann nicht gelöscht werden.");
+                if( ! UserFactory.set_scopeDeleteUser(id)){
+                    alert(MessageFactory.get_error());
                 }
             }
-            
-            UserFactory.set_scopeGridOptions($scope);
-            
-            StorageFactory.set_storeUserScope($scope);
-            $scope.userAutoId = StorageFactory.funcautoId("user");
-        
+            // INIT USER SCOPE
+            UserFactory.set_scopeInit($scope);
         }
     ]);
 }
--- js/app/factory/ng-userFactory.js
+++ js/app/factory/ng-userFactory.js
@@ -1,18 +1,32 @@
 if(typeof GLOBAL__VARS__APP != "undefined" && typeof GLOBAL__VARS__APP.mainApp != "undefined"){
     GLOBAL__VARS__APP.mainApp.factory('UserFactory',[
+        'StorageFactory',
         'MessageFactory',
-        function (MessageFactory) {
-            var get_checkNicknameUnique = function(scope){
+        function (StorageFactory, MessageFactory) {
+            // STORE USER SCOPE
+            var factoryScope = {};
+            /**
+            * get_checkNicknameUnique
+            *
+            * @returns boolean if nickname not unique than false
+            */
+            var get_checkNicknameUnique = function(){
                 var r = true;
-                if(scope.userData.length){
-                    for(var e in scope.userData){
-                        if(scope.userData[e].nickname==scope.user.nickname){r=false;break;}
+                if(factoryScope.userData.length){
+                    for(var e in factoryScope.userData){
+                        if(factoryScope.userData[e].nickname==factoryScope.user.nickname){r=false;break;}
                     }
                 }
                 return r;
             }
-            var set_gridOptions = function (scope) {
-                scope.gridOptionsUser = { 
+            /**
+            * set_gridOptions
+            *
+            * @description set ng-grid gridOptionsUser
+            * @returns void
+            */
+            var set_gridOptions = function () {
+                factoryScope.gridOptionsUser = { 
                     data: 'userData',
                     showGroupPanel: false,
                     columnDefs: [
@@ -23,41 +37,79 @@ if(typeof GLOBAL__VARS__APP != "undefined" && typeof GLOBAL__VARS__APP.mainApp !
                     ]
                 };
             }
-            var set_addUserData = function (scope) {
-                scope.user.id = scope.userAutoId();
-                scope.userData.push(angular.copy(scope.user));
-                set_gridOptions(scope);
-                scope.user = {nickname: '', firstname: '', secondname: '', id: 0};
+            /**
+            * set_addUserData
+            *
+            * @description set new user
+            * @returns void
+            */
+            var set_addUserData = function () {
+                factoryScope.user.id = factoryScope.autoId();
+                factoryScope.userData.push(angular.copy(factoryScope.user));
+                set_gridOptions();
+                factoryScope.user = {nickname: '', firstname: '', secondname: '', id: 0};
             }
-            var set_addUser = function (scope) {
+            /**
+            * set_addUser
+            *
+            * @description valided userform and if ok run add func
+            * @returns boolean if form not valid than false
+            */
+            var set_addUser = function () {
                 var isok = true;
-                if ( ! scope.userForm.$valid) {
+                if ( ! factoryScope.userForm.$valid) {
                     MessageFactory.set_error("Alle Felder benötigen einen Eintrag!");
                     return false;  
                 }
-                if( ! get_checkNicknameUnique(scope)){
+                if( ! get_checkNicknameUnique()){
                     MessageFactory.set_error("Der Nickname existiert schon.");
                     isok = false;
                 }
                 if(isok){
-                    set_addUserData(scope);
+                    set_addUserData();
                 }
                 return isok;
             }
+            /**
+            * set_deleteUser
+            *
+            * @description valided user not in a team and if ok delete
+            * @returns boolean if user in a team than false
+            */
             var set_deleteUser = function (id, scope) {
                 var res = [];
-                for(var e in scope.userData){
-                    if(scope.userData[e].id != id){
-                        res.push(scope.userData[e]);
+                if( StorageFactory.get_storeCheckUserIsInTeam(id) ){
+                    MessageFactory.set_error("Der Spieler ist Mitglied in einem Team und kann nicht gelöscht werden.");
+                    return false;
+                }else{
+                    for(var e in factoryScope.userData){
+                        if(factoryScope.userData[e].id != id){
+                            res.push(factoryScope.userData[e]);
+                        }
                     }
+                    factoryScope.userData = res;
+                    set_gridOptions(); 
+                    StorageFactory.set_storeTeamUserDataScope(factoryScope.userData); 
+                    return true;  
                 }
-                scope.userData = res;
-                set_gridOptions(scope);
+                
+            }
+            /**
+            * set_init
+            *
+            * @description set default scope store
+            * @returns void
+            */
+            var set_init = function(scope){
+                factoryScope = scope;
+                set_gridOptions();
+                StorageFactory.set_storeUserScope(scope);
+                factoryScope.autoId = StorageFactory.funcautoId("user");
             }
             return {
+                set_scopeInit: set_init,
                 set_scopeAddUser: set_addUser,
-                set_scopeDeleteUser: set_deleteUser,
-                set_scopeGridOptions: set_gridOptions
+                set_scopeDeleteUser: set_deleteUser
             };
         }
     ]);
