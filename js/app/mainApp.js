86914ffb371a90131c36b101785fa449a814b8ad
Stefan Chrobak <info@netcodev.de>
2014-10-02 21:21:57 +0200

    Create MainApp Config
    Create mainApp config with stateprovider and ui-router

--- js/app/mainApp.js
+++ js/app/mainApp.js
@@ -1,7 +1,24 @@
 (function() {
     "use strict";
 
-    angular.module('mainApp', ['mainAppFactory', 'ngGrid']);
+    angular.module('mainApp', ['ui.router', 'ngGrid', 'mainAppFactory']);
+    
+    angular.module('mainApp').config(
+        function($stateProvider, $urlRouterProvider){
+            
+            $urlRouterProvider.otherwise("/");
+            
+            $stateProvider
+            .state('index', {
+                url: "",
+                views: {
+                    "viewUser": { templateUrl: "templates/user-template.html" },
+                    "viewTeam": { templateUrl: "templates/team-template.html" },
+                    "viewGame": { templateUrl: "templates/game-template.html" }
+                }
+            });
+        }
+    );
 })();
 
 
